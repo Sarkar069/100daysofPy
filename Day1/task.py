e0f2dd9ad02a517d4c0462f15b340a49c6321229
Sarkar069 <ishuthakur186@gmail.com>
2025-02-18 18:10:28 +0530

    Initial commit with project structure

---
+++ Day1/task.py
@@ -0,0 +1,11 @@
+
+# Band name Generator
+# Using the f string for string concatenation
+
+print("Welcome to the Band Name Generator.")
+city = input("What is the name of the city you grew up in...?\n")
+pet = input("what's your pet's name..?\n")
+
+print("Your band name could be :" + city + " " + pet)
+
+# print(f"Your band name could be: {city} {pet}")
